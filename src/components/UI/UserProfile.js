40cc2520c6a28893e758afc5aa2e90045dfca2a1
Taichi Murakami <64308722+taichimurakami-github@users.noreply.github.com>
2021-12-28 20:50:31 +0900

    userDocのデータをとりあえず反映

--- src/components/UI/UserProfile.js
+++ src/components/UI/UserProfile.js
@@ -3,7 +3,15 @@ export const UserProfile = (props) => {
     <div className="user-profile-wrapper">
       <img className="user-icon" src={props.userDoc?.photo}></img>
       <p className="name">{props.userDoc?.name}</p>
-      <p className="profile card">{props.userDoc?.profile}</p>
+      <p className="name">{props.userDoc?.age + " 歳"}</p>
+      <p className="name">
+        {props.userDoc?.hometown?.prefecture} {props.userDoc?.hometown?.city}{" "}
+        在住
+      </p>
+      <p className="profile">
+        <h3>プロフィール</h3>
+        {props.userDoc?.profile}
+      </p>
     </div>
   );
 };
